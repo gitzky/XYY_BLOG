fe000738d25dba9608da1fdb46f068c95873c520
gitzky <927926261@qq.com>
2018-09-29 08:59:18 +0800

    update-add vendor

--- src/resources/views/Admin/post/postAdd.blade.php
+++ src/resources/views/Admin/post/postAdd.blade.php
@@ -95,14 +95,3 @@
 	<script type="text/javascript" src="/adm/post/postAdd.js"></script>
 </body>
 </html>
-<script>
-layui.use('upload', function(){
-  layui.upload({
-	  url: '/admin/uploadImg'
-	  ,success: function(res){
-	    console.log(res); //上传成功返回值，必须为json格式
-	  }
-	});   
-});
- 
-</script>
